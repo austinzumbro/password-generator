0fd78672e984ef865db74889d24ee6c6e6afe778
Austin Zumbro <azumbro@gmail.com>
2023-03-27 15:09:52 -0700

    set up file structure and begin initial Javascript work establishing character arrays

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Build the character arrays.
+//
+// I don't want to create the arrays by hand, so I'll just
+// include them as strings and iterate over them to make the
+// arrays.
+
+var lowercaseAlphabetString = "abcdefghijklmnopqrstuvwxyz";
+var specialCharacterString = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+var lowercaseAlphabet = [];
+var uppercaseAlphabet = [];
+var specialCharacters = [];
+
+// Iterate over the strings and push the characters onto the arrays.
+
+for (let i = 0; i < lowercaseAlphabetString.length; i++) {
+  lowercaseAlphabet.push(lowercaseAlphabetString[i]);
+  uppercaseAlphabet.push(lowercaseAlphabetString[i].toUpperCase());
+
+  // Check if the iteration worked.
+  // console.log(lowercaseAlphabet[i]);
+  // console.log(uppercaseAlphabet[i]);
+  // It does! Commenting this out.
+}
+
+for (let i = 0; i < specialCharacterString.length; i++) {
+  specialCharacters.push(specialCharacterString[i]);
+
+  // Check if the iteration worked.
+  // console.log(specialCharacters[i]);
+  // It does! Commenting this out.
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
