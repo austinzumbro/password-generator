4f8c23ef51d1e8e3e178fe29a7278b9ff2a7e1e3
Austin Zumbro <austin@Austins-MacBook-Pro.local>
2023-03-29 14:50:25 -0700

    update variable structure to more of an object model

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,36 +8,103 @@ var generateBtn = document.querySelector('#generate');
 // arrays.
 
 var lowercaseAlphabetString = 'abcdefghijklmnopqrstuvwxyz';
+var numericCharacterString = '0123456789';
 var specialCharacterString = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
-var integerCharacterString = '0123456789';
 
 var lowercaseAlphabet = [];
 var uppercaseAlphabet = [];
+var numericCharacters = [];
 var specialCharacters = [];
-var integerCharacters = [];
 
 // Iterate over the strings and push the characters onto the arrays.
 
 for (let i = 0; i < lowercaseAlphabetString.length; i++) {
 	lowercaseAlphabet.push(lowercaseAlphabetString[i]);
 	uppercaseAlphabet.push(lowercaseAlphabetString[i].toUpperCase());
-
-	// Check if the iteration worked.
-	// console.log(lowercaseAlphabet[i]);
-	// console.log(uppercaseAlphabet[i]);
-	// It does! Commenting this out.
 }
 
-for (let i = 0; i < integerCharacterString.length; i++) {
-	integerCharacters.push(integerCharacterString[i]);
+for (let i = 0; i < numericCharacterString.length; i++) {
+	numericCharacters.push(numericCharacterString[i]);
 }
 
 for (let i = 0; i < specialCharacterString.length; i++) {
 	specialCharacters.push(specialCharacterString[i]);
+}
 
-	// Check if the iteration worked.
-	// console.log(specialCharacters[i]);
-	// It does! Commenting this out.
+// Password Criteria
+// Default criteria options.
+var criteria = {
+	characters: [],
+	length: 8,
+	lowercase: true,
+	uppercase: true,
+	numeric: true,
+	special: true,
+	// validateLength: function() {
+	//   if (this.length >= 8 || this.length <= 128) {
+	//     return true;
+	//   } else {
+	//     return false;
+	//   }
+	// }
+	collectUserInput: function () {
+		// reset the character library
+		this.characters = [];
+		// collect the number of characters specification from the user
+		this.length = prompt(
+			'How many characters? 8 character minimum, 128 character maximum.',
+			this.length
+		);
+		// validate the user provided specifications
+		// require them to re-enter a correct value
+		while (this.length <= 8 && this.length >= 128) {
+			this.length = prompt(
+				'Your password must be between 8 and 128 characters in length. Please enter a valid response.'
+			);
+		}
+		// check if lowercase letters should be included
+		// if true, add those characters to the library
+		this.lowercase = prompt(
+			'Please confirm that the password should include lowercase letters.\nIf you do not wish to include these characters, press Cancel.'
+		);
+		if (this.lowercase) {
+			fullCharacterArray.concat(lowercaseAlphabet);
+		}
+		// check if uppercase letters should be included
+		// if true, add those characters to the library
+		this.uppercase = prompt(
+			'Please confirm that the password should include uppercase letters.\nIf you do not wish to include these characters, press Cancel.'
+		);
+		if (this.uppercase) {
+			fullCharacterArray.concat(uppercaseAlphabet);
+		}
+		// check if numeric characters should be included
+		// if true, add those characters to the library
+		this.numeric = prompt(
+			'Please confirm that the password should include numeric characters.\nIf you do not wish to include these characters, press Cancel.'
+		);
+		if (this.numeric) {
+			fullCharacterArray.concat(numericCharacters);
+		}
+		// check if special characters should be included
+		// if true, add those characters to the library
+		this.special = prompt(
+			'Please confirm that the password should include special characters.\nIf you do not wish to include these characters, press Cancel.'
+		);
+		if (this.special) {
+			fullCharacterArray.concat(specialCharacters);
+		}
+	},
+};
+
+function generatePassword() {
+	criteria.collectUserInput;
+	if (fullCharacterArray.length < 1) {
+		alert(
+			'You must include at least one set of characters in order to generate a password.'
+		);
+		return;
+	}
 }
 
 // Write password to the #password input
