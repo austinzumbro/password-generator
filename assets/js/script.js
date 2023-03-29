90bd187ac95f8da5df98a14f763902df010f236a
Austin Zumbro <austin@Austins-MacBook-Pro.local>
2023-03-29 10:33:51 -0700

    add an integer array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
 
 // Build the character arrays.
 //
@@ -7,42 +7,46 @@ var generateBtn = document.querySelector("#generate");
 // include them as strings and iterate over them to make the
 // arrays.
 
-var lowercaseAlphabetString = "abcdefghijklmnopqrstuvwxyz";
-var specialCharacterString = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+var lowercaseAlphabetString = 'abcdefghijklmnopqrstuvwxyz';
+var specialCharacterString = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+var integerCharacterString = '0123456789';
 
 var lowercaseAlphabet = [];
 var uppercaseAlphabet = [];
 var specialCharacters = [];
+var integerCharacters = [];
 
 // Iterate over the strings and push the characters onto the arrays.
 
 for (let i = 0; i < lowercaseAlphabetString.length; i++) {
-  lowercaseAlphabet.push(lowercaseAlphabetString[i]);
-  uppercaseAlphabet.push(lowercaseAlphabetString[i].toUpperCase());
+	lowercaseAlphabet.push(lowercaseAlphabetString[i]);
+	uppercaseAlphabet.push(lowercaseAlphabetString[i].toUpperCase());
 
-  // Check if the iteration worked.
-  // console.log(lowercaseAlphabet[i]);
-  // console.log(uppercaseAlphabet[i]);
-  // It does! Commenting this out.
+	// Check if the iteration worked.
+	// console.log(lowercaseAlphabet[i]);
+	// console.log(uppercaseAlphabet[i]);
+	// It does! Commenting this out.
+}
+
+for (let i = 0; i < integerCharacterString.length; i++) {
+	integerCharacters.push(integerCharacterString[i]);
 }
 
 for (let i = 0; i < specialCharacterString.length; i++) {
-  specialCharacters.push(specialCharacterString[i]);
+	specialCharacters.push(specialCharacterString[i]);
 
-  // Check if the iteration worked.
-  // console.log(specialCharacters[i]);
-  // It does! Commenting this out.
+	// Check if the iteration worked.
+	// console.log(specialCharacters[i]);
+	// It does! Commenting this out.
 }
 
 // Write password to the #password input
 function writePassword() {
-  let 
+	var password = generatePassword();
+	var passwordText = document.querySelector('#password');
 
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+	passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
