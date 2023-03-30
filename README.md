2710dc7edc6007faed81ae8cf1056fcda3123a4a
Austin Zumbro <azumbro@gmail.com>
2023-03-30 16:28:10 -0700

    replace for loops with split() method to create character arrays, update README.md

--- README.md
+++ README.md
@@ -1,1 +1,96 @@
-# password-generator
+# Password Generator
+
+## Description
+
+For this week's challenge, I'd like to explore some basic Javascript. In the process, I'm going to build myself a simple password generator.
+
+| **Scenario**                                                                                                                                                                                                                                                                                                                 |
+| :--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| _The password generator will ask the user to specify the length their desired password. They also should be able to confirm what types of characters to include (lowercase letters, uppercase letters, numeric, or special). Based on that user input, the tool will generate a random sequence and present it on the page._ |
+
+A project like this should give me a chance to play with some basic JS methods. I'm also interested in exploring the Object data type.
+
+Away we go!
+
+---
+
+## My Approach
+
+First things first, I needed to create some "character libraries" from which to randomly pull characters. It makes sense to store these as arrays.
+
+Because I want the user to be able to include or reject character types, I decided to hold them in separate arrays at first, then combine them based on the user's input.
+
+### Building the Arrays
+
+Rather than type out the arrays by hand, I decided to include each "type" as a single string. Then I used the `split()` method to generate the arrays.
+
+To take the "Special Characters" array as an example, I simply copied the string of special characters from [owasp.org](https://owasp.org/www-community/password-special-characters).
+
+![Screenshot of the special character string on owasp.org ](assets/images/owasp-special-characters.png "owasp.org Special Characters")
+
+Then I pasted that string into a variable and ran a `split()` to generate the array.
+
+```javascript
+var specialCharacterString = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+var specialCharacters = specialCharacterString.split("");
+```
+
+### Collecting User Input
+
+I chose to store the user input as an object called `criteria`, the properties of which were the specifications of the to-be-generated password, e.g. `numChars` stores an integer that specifies the number of characters, `lowercase` stores a boolean that dicates whether or not to include lowercase letters, etc.
+
+> In retrospect, I think I should have named this object something a little more clear and descriptive, like `userInput`. In general, "naming variables something clear and concise that you won't regret and want to change later" is a real area of growth for me.
+
+Because I was interested in exploring objects as a Javascript concept, I chose to include the function for collecting the user input as a method on that same object. The method uses a combination of `prompt()` and `confirm()` to assign values to the `criteria` object's properties.
+
+> Specifically, I was interested in getting more familiar with the `this` keyword, and writing the method was a nice way to do that.
+
+As the user confirms each character category, that array gets merged into the `criteria.character` property. If, at the end of the process no categories have been included, the user is alerted and told to start again.
+
+### Generating the Password
+
+Once I got the user input functionality sorted out, it was pretty straightforward to generate the password string. I just ran a `for` loop to build a string of the user-specified length, using the `Math.floor` and `Math.random` methods to grab a random entry in the character library each time.
+
+```javascript
+for (let i = 0; i < criteria.numChars; i++) {
+  passwordString +=
+    criteria.characters[Math.floor(Math.random() * criteria.characters.length)];
+}
+```
+
+## Credits
+
+For this project, my only real outside reference text was the Javascript section over on [MDN Web Docs](https://developer.mozilla.org/en-US/docs/Web/JavaScript). Although, in all likelihood, I probably also looked at [W3Schools](https://www.w3schools.com/jsref/default.asp).
+
+## Learnings
+
+This was a fun way to play around with some basic Javascript, and I do think I got a lot more comfortable with the `this` keyword (and objects in general).
+
+I strongly dislike the `prompt()` and `confirm()` methods as a means of interacting with users. (I don't think this is a hot take, since I almost never see these methods used on other sites.) I would much rather fill out a form to specify my preferences.
+
+After looking into how to make that form a reality, it seems pretty straightforward (use querySelectors to look at elements, etc.), but I decided that fell outside the scope of this particular exercise. I may come back later and update this to make it more fun to use.
+
+## License
+
+MIT License
+
+Copyright (c) 2023 Austin Zumbro
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var generateBtn = document.querySelector('#generate');
+var generateBtn = document.querySelector("#generate");
 
 // Build the character arrays.
 //
@@ -7,160 +7,158 @@ var generateBtn = document.querySelector('#generate');
 // include them as strings and iterate over them to make the
 // arrays.
 
-var lowercaseAlphabetString = 'abcdefghijklmnopqrstuvwxyz';
-var numericCharacterString = '0123456789';
-var specialCharacterString = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+var lowercaseAlphabetString = "abcdefghijklmnopqrstuvwxyz";
+var numericCharacterString = "0123456789";
+var specialCharacterString = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
-var lowercaseAlphabet = [];
-var uppercaseAlphabet = [];
-var numericCharacters = [];
-var specialCharacters = [];
+var lowercaseAlphabet = lowercaseAlphabetString.split("");
+var uppercaseAlphabet = lowercaseAlphabetString.toUpperCase().split("");
+var numericCharacters = numericCharacterString.split("");
+var specialCharacters = specialCharacterString.split("");
 
-// Iterate over the strings and push the characters onto the arrays.
+// OUTDATED CODE!
+// Originally, before I discovered the split() method, I created
+// the arrays using for loops. I am keeping the code below as
+// as a historical reference.
 
-for (let i = 0; i < lowercaseAlphabetString.length; i++) {
-	lowercaseAlphabet.push(lowercaseAlphabetString[i]);
-	uppercaseAlphabet.push(lowercaseAlphabetString[i].toUpperCase());
-}
-
-for (let i = 0; i < numericCharacterString.length; i++) {
-	numericCharacters.push(numericCharacterString[i]);
-}
-
-for (let i = 0; i < specialCharacterString.length; i++) {
-	specialCharacters.push(specialCharacterString[i]);
-}
+// // Iterate over the strings and push the characters onto the arrays.
+//
+// for (let i = 0; i < lowercaseAlphabetString.length; i++) {
+//   lowercaseAlphabet.push(lowercaseAlphabetString[i]);
+//   uppercaseAlphabet.push(lowercaseAlphabetString[i].toUpperCase());
+// }
+//
+// for (let i = 0; i < numericCharacterString.length; i++) {
+//   numericCharacters.push(numericCharacterString[i]);
+// }
+//
+// for (let i = 0; i < specialCharacterString.length; i++) {
+// 	specialCharacters.push(specialCharacterString[i]);
+// }
 
 // Password Criteria
 // Default criteria options.
 var criteria = {
-	characters: [],
-	numChars: 8,
-	lowercase: true,
-	uppercase: true,
-	numeric: true,
-	special: true,
-	// validateLength: function() {
-	//   if (this.length >= 8 || this.length <= 128) {
-	//     return true;
-	//   } else {
-	//     return false;
-	//   }
-	// }
-	collectUserInput: function () {
-		// reset the character library
-		this.characters = [];
-		console.log(this.characters);
+  // initialize the object with default values
+  characters: [],
+  numChars: 8,
+  lowercase: true,
+  uppercase: true,
+  numeric: true,
+  special: true,
+  collectUserInput: function () {
+    // reset the character library in the case of multiple generation attempts
+    this.characters = [];
+    console.log(this.characters);
 
-		// collect the number of characters specification from the user
-		this.numChars = parseInt(
-			prompt(
-				'What length, in characters, would you like your password to be?\nThere is an 8 character minimum and a 128 character maximum.'
-			)
-		);
-		// if user hits "cancel" then stop collecting input
-		if (!this.numChars) {
-			return;
-		}
+    // collect the "number of characters" specification from the user
+    this.numChars = parseInt(
+      prompt(
+        "What length, in characters, would you like your password to be?\nThere is an 8 character minimum and a 128 character maximum."
+      )
+    );
+    // if user hits "cancel" then stop collecting input
+    if (!this.numChars) {
+      return;
+    }
 
-		// validate the user provided specifications
-		// require them to re-enter a correct value
-		while (!(this.numChars >= 8 && this.numChars <= 128)) {
-			this.numChars = prompt(
-				'Your password must be between 8 and 128 characters in length. Please enter a valid response.'
-			);
-			if (!this.length) break;
-		}
-		// if the user hits "cancel" then stop collecting input
-		if (!this.numChars) {
-			return;
-		}
+    // validate the user provided specifications
+    // require them to re-enter a correct value
+    while (!(this.numChars >= 8 && this.numChars <= 128)) {
+      this.numChars = prompt(
+        "Your password must be between 8 and 128 characters in length. Please enter a valid response."
+      );
+      if (!this.length) break;
+    }
+    // if the user hits "cancel" then stop collecting input
+    if (!this.numChars) {
+      return;
+    }
 
-		// check if lowercase letters should be included
-		// if true, add those characters to the library
-		this.lowercase = confirm(
-			'Please confirm that the password should include lowercase letters.\nIf you do not wish to include these characters, press Cancel.'
-		);
-		if (this.lowercase) {
-			this.characters = this.characters.concat(lowercaseAlphabet);
-		}
-		console.log(this.characters);
+    // check if lowercase letters should be included
+    // if true, add those characters to the character library
+    this.lowercase = confirm(
+      "Please confirm that the password should include lowercase letters.\nIf you do not wish to include these characters, press Cancel."
+    );
+    if (this.lowercase) {
+      this.characters = this.characters.concat(lowercaseAlphabet);
+    }
+    console.log(this.characters);
 
-		// check if uppercase letters should be included
-		// if true, add those characters to the library
-		this.uppercase = confirm(
-			'Please confirm that the password should include uppercase letters.\nIf you do not wish to include these characters, press Cancel.'
-		);
-		if (this.uppercase) {
-			this.characters = this.characters.concat(uppercaseAlphabet);
-		}
-		console.log(this.characters);
+    // check if uppercase letters should be included
+    // if true, add those characters to the character library
+    this.uppercase = confirm(
+      "Please confirm that the password should include uppercase letters.\nIf you do not wish to include these characters, press Cancel."
+    );
+    if (this.uppercase) {
+      this.characters = this.characters.concat(uppercaseAlphabet);
+    }
+    console.log(this.characters);
 
-		// check if numeric characters should be included
-		// if true, add those characters to the library
-		this.numeric = confirm(
-			'Please confirm that the password should include numeric characters.\nIf you do not wish to include these characters, press Cancel.'
-		);
-		if (this.numeric) {
-			this.characters = this.characters.concat(numericCharacters);
-		}
-		console.log(this.characters);
+    // check if numeric characters should be included
+    // if true, add those characters to the character library
+    this.numeric = confirm(
+      "Please confirm that the password should include numeric characters.\nIf you do not wish to include these characters, press Cancel."
+    );
+    if (this.numeric) {
+      this.characters = this.characters.concat(numericCharacters);
+    }
+    console.log(this.characters);
 
-		// check if special characters should be included
-		// if true, add those characters to the library
-		this.special = confirm(
-			'Please confirm that the password should include special characters.\nIf you do not wish to include these characters, press Cancel.'
-		);
-		if (this.special) {
-			this.characters = this.characters.concat(specialCharacters);
-		}
-		console.log(this.characters);
-	},
+    // check if special characters should be included
+    // if true, add those characters to the character library
+    this.special = confirm(
+      "Please confirm that the password should include special characters.\nIf you do not wish to include these characters, press Cancel."
+    );
+    if (this.special) {
+      this.characters = this.characters.concat(specialCharacters);
+    }
+    console.log(this.characters);
+  },
 };
 
 function generatePassword() {
-	// initialize a string
-	var passwordString = '';
+  // initialize a string
+  var passwordString = "";
 
-	// collect the user input regarding length, character library, etc.
-	criteria.collectUserInput();
+  // collect the user input regarding length, character library, etc.
+  criteria.collectUserInput();
 
-	// if user hits "cancel," abort the process and return nothing
-	if (!criteria.numChars) {
-		return null;
-	}
+  // if user hits "cancel," abort the process and return nothing
+  if (!criteria.numChars) {
+    return null;
+  }
 
-	// if the user does not approve any character libraries, alert them to try again and return nothing
-	if (criteria.characters.length < 1) {
-		alert(
-			'You must include at least one set of characters in order to generate a password.\nPlease try again and confirm at least one set of character types.'
-		);
-		return null;
-	}
+  // if the user does not approve any character libraries, alert them to try again and return nothing
+  if (criteria.characters.length < 1) {
+    alert(
+      "You must include at least one set of characters in order to generate a password.\nPlease try again and confirm at least one set of character types."
+    );
+    return null;
+  }
 
-	if (criteria.characters.length > 0) {
-		console.log('if statement works');
-	}
+  // Pull a random character out of the character library and append
+  // it to the password string until the desired password length is
+  // reached.
+  for (let i = 0; i < criteria.numChars; i++) {
+    passwordString +=
+      criteria.characters[
+        Math.floor(Math.random() * criteria.characters.length)
+      ];
+  }
 
-	for (let i = 0; i < criteria.numChars; i++) {
-		console.log('For Loop is working.');
-		passwordString +=
-			criteria.characters[
-				Math.floor(Math.random() * criteria.characters.length)
-			];
-	}
-
-	return passwordString;
+  // return the complete password string
+  return passwordString;
 }
 
 // Write password to the #password input
 function writePassword() {
-	var password = generatePassword();
-	var passwordText = document.querySelector('#password');
-	console.log(password);
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  console.log(password);
 
-	passwordText.value = password;
+  passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener("click", writePassword);
